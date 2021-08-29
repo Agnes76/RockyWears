77766bc2cd101cec1b22cc59085ac22f7cde810a
Agnes76 <ugochukwuagnes20@gmail.com>
2021-08-29 22:26:31 +0100

    Fixing CICD

--- Database/DataSeeder.cs
+++ Database/DataSeeder.cs
@@ -32,24 +32,7 @@ namespace Database
                     var productList = JsonConvert.DeserializeObject<List<Product>>(productData);
                     await context.Products.AddRangeAsync(productList);
                 }
-                //if (!context.Contacts.Any())
-                //{
-                //    var contactData = File.ReadAllText(Path.Combine(dirDb , "json","contact.json"));
-                //    var contactList = JsonConvert.DeserializeObject<List<Contact>>(contactData);
-                //    await context.Contacts.AddRangeAsync(contactList);
-                //}
-                //if (!context.Categories.Any())
-                //{
-                //    var categoryData = File.ReadAllText(Path.Combine(dirDb ,"json","category.json"));
-                //    var categoryList = JsonConvert.DeserializeObject<List<Category>>(categoryData);
-                //    await context.Categories.AddRangeAsync(categoryList);
-                //}
-                //if (!context.Images.Any())
-                //{
-                //    var imageData = File.ReadAllText(Path.Combine(dirDb + "json","image.json"));
-                //    var imageList = JsonConvert.DeserializeObject<List<Image>>(imageData);
-                //    await context.Images.AddRangeAsync(imageList);
-                //}
+                
                 await context.SaveChangesAsync();
             }
             catch (Exception ex)
